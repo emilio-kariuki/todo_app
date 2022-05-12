3af65ceef584eae409057569cd10f202ce75cb3e
Emilio Kariuki <emili0113kariuki@gmail.com>
2022-05-12 12:41:35 +0300

    Added new files to the repositry

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/db/db_helper.dart';
+import 'package:todo_app/services/theme_service.dart';
+import 'package:todo_app/ui/home_page.dart';
+import 'package:todo_app/ui/theme.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DBHelper.initDb();
+  await GetStorage.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeService().theme,
+      home: const HomePage(),
+    );
+  }
+}
